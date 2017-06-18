b65bd0477745f70e1cbf931ecba497b572ad79bb
林博仁 <Buo.Ren.Lin@gmail.com>
2017-06-18 19:43:56 +0800

    Start tracking
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ README.markdown
@@ -0,0 +1,18 @@
+# xml.bash
+A GNU Bash wrapper and library hibrid for XML markup processing(you may use it as a standalone command, or source it and utilize it in GNU Bash scripts)  
+<https://github.com/Lin-Buo-Ren/xml.bash>
+
+NOTE: This program is in early development stage, only library part is currently implemented.
+
+## 原作者<br>Original Author
+林博仁 &lt;<Buo.Ren.Lin@gmail.com>&gt;
+
+## 智慧財產授權條款<br>Intellectual Property License
+[GNU GPL v3+](http://creativecommons.org/licenses/by-sa/3.0/)
+
+```
+這個專案介紹文件是基於專案介紹文件範本
+This README is based on Project README Template
+
+http://github.com/Lin-Buo-Ren/Project-README-templates
+```
---
+++ xml.bash
@@ -0,0 +1,187 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2034
+
+## Makes debuggers' life easier - Unofficial Bash Strict Mode
+## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+## Non-overridable Primitive Variables
+## BASHDOC: Shell Variables » Bash Variables
+## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
+if [ -v "BASH_SOURCE[0]" ]; then
+	RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
+	RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+	RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
+	declare -r\
+		RUNTIME_EXECUTABLE_FILENAME\
+		RUNTIME_EXECUTABLE_DIRECTORY\
+		RUNTIME_EXECUTABLE_PATHABSOLUTE\
+		RUNTIME_COMMANDLINE_BASECOMMAND
+fi
+declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
+
+## init function: entrypoint of main program
+## This function is called near the end of the file,
+## with the script's command-line parameters as arguments
+_xml_bash_init(){
+	if ! process_commandline_parameters; then
+		printf\
+			"Error: %s: Invalid command-line parameters.\n"\
+			"${FUNCNAME[0]}"\
+			1>&2
+		print_help
+		exit 1
+	fi
+
+	exit 0
+}; declare -fr _xml_bash_init
+
+_xml_bash_create_temp_file(){
+	mktemp\
+			--tmpdir\
+			bash.xml.XXXXXX.xml
+}
+
+xml_delete_node(){
+	local -r xml_file="$1"; shift
+	local -r node_xpath="$1"
+
+	local temp_file
+	temp_file="$(_xml_bash_create_temp_file)"; local -r temp_file
+
+	xmlstarlet\
+		edit\
+			--pf\
+			--ps\
+			--delete\
+			"${node_xpath}"\
+			"${xml_file}"\
+			>"${temp_file}"
+
+	mv\
+		--force\
+		"${temp_file}"\
+		"${xml_file}"
+}
+
+xml_transform_node(){
+	local xsl_file="$1"; shift
+	local xml_file="$1"
+
+	local temp_file
+	temp_file="$(_xml_bash_create_temp_file)"; local -r temp_file
+
+	xmlstarlet\
+		transform\
+			"${xsl_file}"\
+			"${xml_file}"\
+			>"${temp_file}"
+
+	mv\
+		--force\
+		"${temp_file}"\
+		"${xml_file}"
+}
+
+xml_format_node(){
+	local xml_file="$1"
+
+	local temp_file
+	temp_file="$(_xml_bash_create_temp_file)"; local -r temp_file
+
+	xmlstarlet\
+		format\
+			--indent-tab\
+			"${xml_file}"\
+			>"${temp_file}"
+
+	mv\
+		--force\
+		"${temp_file}"\
+		"${xml_file}"
+}
+
+## Traps: Functions that are triggered when certain condition occurred
+## Shell Builtin Commands » Bourne Shell Builtins » trap
+_xml_bash_trap_errexit(){
+	printf "An error occurred and the script is prematurely aborted\n" 1>&2
+	return 0
+}; declare -fr _xml_bash_trap_errexit
+
+_xml_bash_trap_exit(){
+	return 0
+}; declare -fr _xml_bash_trap_exit
+
+_xml_bash_trap_return(){
+	local returning_function="${1}"
+
+	printf "DEBUG: %s: returning from %s\n" "${FUNCNAME[0]}" "${returning_function}" 1>&2
+}; declare -fr _xml_bash_trap_return
+
+_xml_bash_trap_interrupt(){
+	printf "Recieved SIGINT, script is interrupted.\n" 1>&2
+	return 0
+}; declare -fr _xml_bash_trap_interrupt
+
+_xml_bash_print_help(){
+	printf "Currently no help messages are available for this program\n" 1>&2
+	return 0
+}; declare -fr _xml_bash_print_help;
+
+_xml_bash_process_commandline_parameters() {
+	if [ "${#RUNTIME_COMMANDLINE_PARAMETERS[@]}" -eq 0 ]; then
+		return 0
+	fi
+
+	# modifyable parameters for parsing by consuming
+	local -a parameters=("${RUNTIME_COMMANDLINE_PARAMETERS[@]}")
+
+	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
+	local enable_debug=N
+
+	while true; do
+		if [ "${#parameters[@]}" -eq 0 ]; then
+			break
+		else
+			case "${parameters[0]}" in
+				"--help"\
+				|"-h")
+					print_help;
+					exit 0
+					;;
+				"--debug"\
+				|"-d")
+					enable_debug="Y"
+					;;
+				*)
+					printf "ERROR: Unknown command-line argument \"%s\"\n" "${parameters[0]}" >&2
+					return 1
+					;;
+			esac
+			# shift array by 1 = unset 1st then repack
+			unset "parameters[0]"
+			if [ "${#parameters[@]}" -ne 0 ]; then
+				parameters=("${parameters[@]}")
+			fi
+		fi
+	done
+
+	if [ "${enable_debug}" = "Y" ]; then
+		trap 'trap_return "${FUNCNAME[0]}"' RETURN
+		set -o xtrace
+	fi
+	return 0
+}; declare -fr _xml_bash_process_commandline_parameters;
+
+# init "${@}"
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+declare -r META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.26.0-32-g317af27-dirty"
+## You may rebase your script to incorporate new features and fixes from the template
